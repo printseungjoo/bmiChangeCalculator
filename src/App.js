dfe9df66d164b9d3c97b99e6fd17254e7d2980cf
printseungjoo <printseungjoo@gmail.com>
2025-06-02 17:41:02 +0900

    functionsCompleted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
 
   const [height, setHeight] = useState();
   const [weight, setWeight] = useState();
   const [bmiStatus, setBmiStatus] = useState('');
-  const [maxOrMin, setMaxOrMin] = useState('최소 / 최대');
-  const [diffWeight, setDiffWeight] = useState();
   const [moreOrLess, setMoreOrLess] = useState('늘려야 / 빼야');
   const [pigBox, setPigBox] = useState();
+  const [leftPigBox, setLeftPigBox] = useState(0);
+  const [rightPigBox, setRightPigBox] = useState(0);
+  const [moreValue, setMoreValue] = useState(0);
+
+  const originalHeight = useRef(0);
+  const originalWeight = useRef(0);
+  const originalPigBox = useRef(0);
+  const originalBMIValue = useRef(0);
+
+  const minOrMaxValue = [
+    {
+      id: 1,
+      min: -1,
+      max: 15.99
+    },
+    {
+      id: 2,
+      min: 16,
+      max: 16.99
+    },
+    {
+      id: 3,
+      min: 17,
+      max: 18.49
+    },
+    {
+      id: 4,
+      min: 18.5,
+      max: 24.99
+    },
+    {
+      id: 5,
+      min: 25,
+      max: 29.99
+    },
+    {
+      id: 6,
+      min: 30,
+      max: 34.99
+    },
+    {
+      id: 7,
+      min: 35,
+      max: 39.99
+    },
+    {
+      id: 8,
+      min: 40,
+      max: -1
+    }
+  ]
 
   function consoleFunction() {
     console.log(height);
     console.log(weight);
   }
   consoleFunction();
-
-
 
   useEffect(() => {
     if (height != undefined && weight != undefined) {
@@ -58,8 +105,40 @@ function App() {
         setPigBox(8);
       }
       document.getElementById('status').innerHTML = bmiStatus;
+      originalHeight.current = height;
+      originalWeight.current = weight;
+      originalPigBox.current = pigBox;
+      originalBMIValue.current = bmiValue;
     }
   }, [height, weight, bmiStatus]);
+
+  function leftClicked() {
+    if (pigBox > 1) {
+      setPigBox(pigBox - 1)
+    }
+  }
+
+  function rightClicked() {
+    if (pigBox < 8) {
+      setPigBox(pigBox + 1)
+    }
+  }
+
+  useEffect(() => {
+    if (height !== undefined && weight !== undefined) {
+      if (pigBox < originalPigBox.current) {
+        const newMoreValue = Math.ceil((originalBMIValue.current - minOrMaxValue[pigBox - 1].max) * originalHeight.current * originalHeight.current / 10000);
+        setMoreOrLess('빼야');
+        setMoreValue(newMoreValue);
+      } else if (pigBox > originalPigBox.current) {
+        const newMoreValue = Math.ceil((minOrMaxValue[pigBox - 1].min - originalBMIValue.current) * originalHeight.current * originalHeight.current / 10000);
+        setMoreOrLess('늘려야');
+        setMoreValue(newMoreValue);
+      } else {
+        setMoreValue(0);
+      }
+    }
+  }, [pigBox]);
 
   return (
     <div className="App">
@@ -84,7 +163,7 @@ function App() {
               <p className="colorBoxP"> 심한 마름 </p>
             </div>
             <div className="colorBoxUnder" id="colorBox1Under">
-              {pigBox === 1 && <img src="../pig.png" alt="돼지" className="pigClass"/>}
+              {pigBox === 1 && <img src="../pig.png" alt="돼지" className="pigClass" />}
             </div>
           </div>
           <div id="colorBox2">
@@ -92,7 +171,7 @@ function App() {
               <p className="colorBoxP"> 많이 마름 </p>
             </div>
             <div className="colorBoxUnder" id="colorBox2Under">
-            {pigBox === 2 && <img src="../pig.png" alt="돼지" className="pigClass"/>}
+              {pigBox === 2 && <img src="../pig.png" alt="돼지" className="pigClass" />}
             </div>
           </div>
           <div id="colorBox3">
@@ -100,7 +179,7 @@ function App() {
               <p className="colorBoxP"> 조금 마름 </p>
             </div>
             <div className="colorBoxUnder" id="colorBox3Under">
-            {pigBox === 3 && <img src="../pig.png" alt="돼지" className="pigClass"/>}
+              {pigBox === 3 && <img src="../pig.png" alt="돼지" className="pigClass" />}
             </div>
           </div>
           <div id="colorBox4">
@@ -108,7 +187,7 @@ function App() {
               <p className="colorBoxP"> 보통 체중 </p>
             </div>
             <div className="colorBoxUnder" id="colorBox4Under">
-            {pigBox === 4 && <img src="../pig.png" alt="돼지" className="pigClass"/>}
+              {pigBox === 4 && <img src="../pig.png" alt="돼지" className="pigClass" />}
             </div>
           </div>
           <div id="colorBox5">
@@ -116,7 +195,7 @@ function App() {
               <p className="colorBoxP"> 약간 과체중 </p>
             </div>
             <div className="colorBoxUnder" id="colorBox5Under">
-            {pigBox === 5 && <img src="../pig.png" alt="돼지" className="pigClass"/>}
+              {pigBox === 5 && <img src="../pig.png" alt="돼지" className="pigClass" />}
             </div>
           </div>
           <div id="colorBox6">
@@ -124,7 +203,7 @@ function App() {
               <p className="colorBoxP"> 비만 </p>
             </div>
             <div className="colorBoxUnder" id="colorBox6Under">
-            {pigBox === 6 && <img src="../pig.png" alt="돼지" className="pigClass"/>}
+              {pigBox === 6 && <img src="../pig.png" alt="돼지" className="pigClass" />}
             </div>
           </div>
           <div id="colorBox7">
@@ -132,7 +211,7 @@ function App() {
               <p className="colorBoxP"> 심한 비만 </p>
             </div>
             <div className="colorBoxUnder" id="colorBox7Under">
-            {pigBox === 7 && <img src="../pig.png" alt="돼지" className="pigClass"/>}
+              {pigBox === 7 && <img src="../pig.png" alt="돼지" className="pigClass" />}
             </div>
           </div>
           <div id="colorBox8">
@@ -140,27 +219,27 @@ function App() {
               <p className="colorBoxP"> 아주 심한 비만 </p>
             </div>
             <div className="colorBoxUnder" id="colorBox8Under">
-            {pigBox === 8 && <img src="../pig.png" alt="돼지" className="pigClass"/>}
+              {pigBox === 8 && <img src="../pig.png" alt="돼지" className="pigClass" />}
             </div>
           </div>
         </div>
         <div id="directionButtons">
-          <button id="leftTriangle">
+          <button id="leftTriangle" onClick={leftClicked}>
           </button>
-          <button id="leftSquare">
+          <button id="leftSquare" onClick={leftClicked}>
           </button>
           <div id="centerBox">
-            <p id="centerBoxP"> 해당 단계가 되려면 {maxOrMin} {diffWeight}kg를 더 {moreOrLess} 합니다. </p>
+            <p id="centerBoxP"> 해당 단계가 되려면 최소 {moreValue}kg를 더 {moreOrLess} 합니다. </p>
           </div>
-          <button id="leftSquare">
+          <button id="rightSquare" onClick={rightClicked}>
           </button>
-          <button id="rightTriangle">
+          <button id="rightTriangle" onClick={rightClicked}>
           </button>
         </div>
-        <p id="who"> BMI 계산은 WHO(세계보건기구)의 기준을 따르고 있습니다. </p>
+        <p id="who"> BMI 계산은 WHO(세계보건기구)의 기준을 따르고 있습니다. 해당 단계까지 가야하는 몸무게 차이의 소수점은 올림되어 정수로 표시됩니다. </p>
       </div>
       <div id="footer">
-          <p> © 2025 함승주. All rights reserved. </p>
+        <p> © 2025 함승주. All rights reserved. </p>
       </div>
     </div>
   );
